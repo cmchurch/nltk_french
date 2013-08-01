67f8c31ad3b60e208e130f2c5721e02cebb44a4e
Chris Church <cmchurch@berkeley.edu>
2013-07-31 23:53:37 -0700

    Added an implementation of OS Walk, REGEX, and NLTK to look at Collocations in one year worth of newspaper data

---
+++ newspaper-le-temps_nltk.py
@@ -0,0 +1,67 @@
+#this python file goes through the xml doccuments and prints out the text data for a particular element name
+#Christopher M. Church
+#PhD Candidate, UC Berkeley, History
+#Social Science D-Lab, UC Berkeley
+
+#USE REGEX INSTEAD OF XML PARSE; lower overhead; also, the files themselves had some problems in the XML and were not properly formed, which resulted in a parse error exception; regex is agnostic to this problem
+import re
+
+#variables
+path="C:\\Users\\Christopher\\Desktop\\1890\\edit" #set the path that we are going to read through
+elem_name = "span" #name of the dom element to look for
+xmls=[] #open up an empty array into which we will store our XML filenames
+output_file="\\all-text.txt"
+
+#get filenames
+for root,dirs,files in os.walk(path): #walk through the filepath and look for xml files, storing them in xmls array
+    for file in files:
+        if file.endswith('.html'):
+            xmls.append(file)
+
+output=open(path+output_file,"w") #OPEN AN OUTPUT FILE FOR ALL THE REGEX MATCHES
+for xml in xmls: #iterate over the files found in the os path walk
+    print xml, #for debugging, print what xml we are on
+    f = open(path + "\\" + xml, "r") #open the xml (xhtml)
+    text = f.read() #read it as raw text
+    regex = re.compile('(<span class="PAG.*?>)(.*?)(</span>)') #create a regular expression with three groups (<span>) (VALUE) (</span>)
+    matches = regex.findall(text) #create a list of tuples that contain our three matched regular expressions
+    for match in matches: output.write(match[1]+" ") #save only the second group (the value) and delimit it with a space so we can tokenize later 
+    f.close() #close the xml (xhtml)
+output.close() #close the output file)
+
+#create an nltk text
+import nltk
+filepath="C:\\Users\\Christopher\\Desktop\\1890\\edit\\all-text.txt" #set the path to the file we want
+f = open(filepath) #open the infile (the output generated from above)
+raw = f.read().decode('utf8') #decode it since it's in UTF8
+f.close() #close the infile
+tokens = nltk.word_tokenize(raw) #tokenize the raw text
+text = nltk.Text(tokens,"utf8") #create an NLTK object from the class "Text" known as "text"
+
+#PRINT THE CONCORDANCE OF THE WORD MARTINIQUE
+text.concordance("Martinique")
+
+
+# NLTK COLLOCATIONS
+from nltk.collocations import *
+bigram_measures = nltk.collocations.BigramAssocMeasures()
+
+words=[w.lower() for w in tokens if w.isalpha()] #filter out all punctuation and numbers
+finder = BigramCollocationFinder.from_words(words) #create a Bigram Collocation Finder
+finder.apply_freq_filter(3) #filter out all words that appear less than 3 times
+#the following stopwords are vernonis stopwords
+french_stopwords = ["Ap.", "Apr.", "GHz", "MHz", "USD", "a", "afin", "ah", "ai", "aie", "aient", "aies", "ait", "alors", "après", "as", "attendu", "au", "au-delà", "au-devant", "aucun", "aucune", "audit", "auprès", "auquel", "aura", "aurai", "auraient", "aurais", "aurait", "auras", "aurez", "auriez", "aurions", "aurons", "auront", "aussi", "autour", "autre", "autres", "autrui", "aux", "auxdites", "auxdits", "auxquelles", "auxquels", "avaient", "avais", "avait", "avant", "avec", "avez", "aviez", "avions", "avons", "ayant", "ayez", "ayons", "b", "bah", "banco", "ben", "bien", "bé", "c", "c'", "c'est", "c'était", "car", "ce", "ceci", "cela", "celle", "celle-ci", "celle-là", "celles", "celles-ci", "celles-là", "celui", "celui-ci", "celui-là", "celà", "cent", "cents", "cependant", "certain", "certaine", "certaines", "certains", "ces", "cet", "cette", "ceux", "ceux-ci", "ceux-là", "cf.", "cg", "cgr", "chacun", "chacune", "chaque", "chez", "ci", "cinq", "cinquante", "cinquante-cinq", "cinquante-deux", "cinquante-et-un", "cinquante-huit", "cinquante-neuf", "cinquante-quatre", "cinquante-sept", "cinquante-six", "cinquante-trois", "cl", "cm", "cm²", "comme", "contre", "d", "d'", "d'après", "d'un", "d'une", "dans", "de", "depuis", "derrière", "des", "desdites", "desdits", "desquelles", "desquels", "deux", "devant", "devers", "dg", "différentes", "différents", "divers", "diverses", "dix", "dix-huit", "dix-neuf", "dix-sept", "dl", "dm", "donc", "dont", "douze", "du", "dudit", "duquel", "durant", "dès", "déjà", "e", "eh", "elle", "elles", "en", "en-dehors", "encore", "enfin", "entre", "envers", "es", "est", "et", "eu", "eue", "eues", "euh", "eurent", "eus", "eusse", "eussent", "eusses", "eussiez", "eussions", "eut", "eux", "eûmes", "eût", "eûtes", "f", "fait", "fi", "flac", "fors", "furent", "fus", "fusse", "fussent", "fusses", "fussiez", "fussions", "fut", "fûmes", "fût", "fûtes", "g", "gr", "h", "ha", "han", "hein", "hem", "heu", "hg", "hl", "hm", "hm³", "holà", "hop", "hormis", "hors", "huit", "hum", "hé", "i", "ici", "il", "ils", "j", "j'", "j'ai", "j'avais", "j'étais", "jamais", "je", "jusqu'", "jusqu'au", "jusqu'aux", "jusqu'à", "jusque", "k", "kg", "km", "km²", "l", "l'", "l'autre", "l'on", "l'un", "l'une", "la", "laquelle", "le", "lequel", "les", "lesquelles", "lesquels", "leur", "leurs", "lez", "lors", "lorsqu'", "lorsque", "lui", "lès", "m", "m'", "ma", "maint", "mainte", "maintes", "maints", "mais", "malgré", "me", "mes", "mg", "mgr", "mil", "mille", "milliards", "millions", "ml", "mm", "mm²", "moi", "moins", "mon", "moyennant", "mt", "m²", "m³", "même", "mêmes", "n", "n'avait", "n'y", "ne", "neuf", "ni", "non", "nonante", "nonobstant", "nos", "notre", "nous", "nul", "nulle", "nº", "néanmoins", "o", "octante", "oh", "on", "ont", "onze", "or", "ou", "outre", "où", "p", "par", "par-delà", "parbleu", "parce", "parmi", "pas", "passé", "pendant", "personne", "peu", "plus", "plus_d'un", "plus_d'une", "plusieurs", "pour", "pourquoi", "pourtant", "pourvu", "près", "puisqu'", "puisque", "q", "qu", "qu'", "qu'elle", "qu'elles", "qu'il", "qu'ils", "qu'on", "quand", "quant", "quarante", "quarante-cinq", "quarante-deux", "quarante-et-un", "quarante-huit", "quarante-neuf", "quarante-quatre", "quarante-sept", "quarante-six", "quarante-trois", "quatorze", "quatre", "quatre-vingt", "quatre-vingt-cinq", "quatre-vingt-deux", "quatre-vingt-dix", "quatre-vingt-dix-huit", "quatre-vingt-dix-neuf", "quatre-vingt-dix-sept", "quatre-vingt-douze", "quatre-vingt-huit", "quatre-vingt-neuf", "quatre-vingt-onze", "quatre-vingt-quatorze", "quatre-vingt-quatre", "quatre-vingt-quinze", "quatre-vingt-seize", "quatre-vingt-sept", "quatre-vingt-six", "quatre-vingt-treize", "quatre-vingt-trois", "quatre-vingt-un", "quatre-vingt-une", "quatre-vingts", "que", "quel", "quelle", "quelles", "quelqu'", "quelqu'un", "quelqu'une", "quelque", "quelques", "quelques-unes", "quelques-uns", "quels", "qui", "quiconque", "quinze", "quoi", "quoiqu'", "quoique", "r", "revoici", "revoilà", "rien", "s", "s'", "sa", "sans", "sauf", "se", "seize", "selon", "sept", "septante", "sera", "serai", "seraient", "serais", "serait", "seras", "serez", "seriez", "serions", "serons", "seront", "ses", "si", "sinon", "six", "soi", "soient", "sois", "soit", "soixante", "soixante-cinq", "soixante-deux", "soixante-dix", "soixante-dix-huit", "soixante-dix-neuf", "soixante-dix-sept", "soixante-douze", "soixante-et-onze", "soixante-et-un", "soixante-et-une", "soixante-huit", "soixante-neuf", "soixante-quatorze", "soixante-quatre", "soixante-quinze", "soixante-seize", "soixante-sept", "soixante-six", "soixante-treize", "soixante-trois", "sommes", "son", "sont", "sous", "soyez", "soyons", "suis", "suite", "sur", "sus", "t", "t'", "ta", "tacatac", "tandis", "te", "tel", "telle", "telles", "tels", "tes", "toi", "ton", "toujours", "tous", "tout", "toute", "toutefois", "toutes", "treize", "trente", "trente-cinq", "trente-deux", "trente-et-un", "trente-huit", "trente-neuf", "trente-quatre", "trente-sept", "trente-six", "trente-trois", "trois", "très", "tu", "u", "un", "une", "unes", "uns", "v", "vers", "via", "vingt", "vingt-cinq", "vingt-deux", "vingt-huit", "vingt-neuf", "vingt-quatre", "vingt-sept", "vingt-six", "vingt-trois", "vis-à-vis", "voici", "voilà", "vos", "votre", "vous", "w", "x", "y", "z", "zéro", "à", "ç'", "ça", "ès", "étaient", "étais", "était", "étant", "étiez", "étions", "été", "étée", "étées", "étés", "êtes", "être", "ô"]
+finder.apply_word_filter(lambda w: w in french_stopwords) #filter out the french stopwords
+
+scored = finder.score_ngrams(bigram_measures.raw_freq) #score them by raw frequency
+sorted(bigram for bigram, score in scored) #sort them by frequency
+
+f = open("bigrams.txt","w") #open an output file
+f.write("SOURCE\tTARGET\tWEIGHT") #write headers
+for bigram in scored: #iterate over each bigram
+   for word in bigram[0]: #since the bigram is a tuple
+        print word,'\t',  #print each word from the tuple
+   f.write(bigram[1]) #then print the score
+   f.write("\n") #print a new line
+f.close() #close the output file
+
